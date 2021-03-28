f09d36636b14dfaf408aba32d018180ad50685c0
Danylov Dmytro <webstorybegin@gmail.com>
2021-03-28 04:27:45 +0300

    counter slider

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ $('.goodFace__slider-top').slick({
   slidesToShow: 3,
   focusOnSelect: true,
   infinite: false,
-  arrows: true
+  arrows: true,
+
 });
 
 $('.goodFace__slider-bottom').slick({
@@ -11,5 +12,20 @@ $('.goodFace__slider-bottom').slick({
   arrows: false,
   fade: true,
   infinite: false,
-  cssEase: 'linear',
+});
+
+let $counter = $('.slideCounter');
+let $slickElement = $('.goodFace__slider-bottom');
+
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide) {
+  let index = (currentSlide ? currentSlide : 0) + 1;
+  $counter.text(index + ' — ' + slick.slideCount);
+  let disabledBtn = document.getElementsByClassName('.slick-next ')
+  if (index == 5) {
+    disabledBtn.classList.remove('slick-disabled')
+  }
+});
+
+$slickElement.slick({
+  autoplay: true,
 });
